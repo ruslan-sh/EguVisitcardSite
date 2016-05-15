a50ef8623c8f5558d9f0fe1ca43933489a97f3b5
Руслан Шохирев <ruslan-sh@taxnet.ru>
2016-05-15 17:24:10 +0300

    Добавлена английская локализация
    - Редактура хроник за октябрь 3301

---
+++ js/index.localization.js
@@ -0,0 +1,176 @@
+$('#selectLanguageDropdown').localizationTool({
+    'defaultLanguage' : 'ru_RU', /* (optional) although must be defined if you don't want en_GB */
+    'showFlag': true,            /* (optional) show/hide the flag */
+    'showCountry': false,         /* (optional) show/hide the country name */
+    'showLanguage': false,        /* (optional) show/hide the country language */    
+    /* 
+     * Translate your strings below
+     */
+    'strings' : {
+        'element:title' : {
+            'en_GB' : 'Welcome to the EG Union and EG Pilots community website'            
+        },
+
+        'Вступить в EG Pilots': {
+            'en_GB' : 'Join EG Pilots'
+        },
+        'Чат EG Pilots' : {
+            'en_GB' : 'EG Pilots Discord'
+        },
+        
+        'id:nav-home' : {
+            'en_GB' : 'HOME'
+        },
+        'id:nav-about' : {
+            'en_GB' : 'ABOUT US'
+        },
+        'id:nav-features' : {
+            'en_GB' : 'EGU ADVANTAGES'
+        },
+        'id:nav-news' : {
+            'en_GB' : 'NEWS'
+        },
+        'id:nav-cmdrs' : {
+            'en_GB' : 'EG PILOTS'
+        },
+        'id:nav-links' : {
+            'en_GB' : 'LINKS'
+        },
+
+        'id:slogan' : {
+            'en_GB' : 'Freedom Among the Stars'
+        },
+        'id:welcome-message' : {
+            'en_GB' : 'Welcome to the EG Union and EG Pilots community website'
+        },
+        
+        
+        'id:about-header': {
+            'en_GB' : 'About us'
+        },        
+        'id:about-message-1': {
+            'en_GB' : 'EG Pilots is a community of free pilots in the world of Elite: Dangerous.'
+        },        
+        'id:about-message-2-1': {
+            'en_GB' : 'EG means '
+        },
+        'id:about-message-2-2': {
+            'en_GB' : ' – it is our community website, dedicated to space related games and everything about space.'
+        },
+        'id:about-message-3': {
+            'en_GB' : 'We are expanding our nominative fraction EG Union, in order to make it one of the dominating Galaxy\'s Power!'
+        },
+
+        'id:about-traders-header': {
+            'en_GB' : 'We are Traders'
+        },
+        'id:about-traders-message': {
+            'en_GB' : 'Here at EG hundreds of space traders from all corners of the inhabited galaxy have found their harbour. Convenient location of our fraction on the borders of Powers makes it attractive for enterpreneurs of all levels. Join us, and we will show you the most profitable and safe trade routes.'
+        },
+        'id:about-fighters-header': {
+            'en_GB' : 'We are Fighters'
+        },
+        'id:about-fighters-message': {
+            'en_GB' : 'Our history is a history of war. War with unwelcome guests, war for our sovereignty, war for our future. The Forge of War have hardened us as a soldiers, ready to be deployed on the first call of our Leader, ready to crush our enemies with an enourmous power of our Armada. Join us now and we will give you the most important thing – purpose! Purpose for fight and for the living!'
+        },
+        'id:about-explorers-header': {
+            'en_GB' : 'We are Explorers'
+        },
+        'id:about-explorers-message': {
+            'en_GB' : 'Everyday dozens of ships departs from our system into unknown parts of the Universe. Join the Expeditions, fly with our pilots for thousands of light years, inscribe you name in the Hall of Pioneer\'s Glory! Ad Astra, CMDR!'
+        },
+        'id:about-hunters-header': {
+            'en_GB' : 'We are Hunters'
+        },
+        'id:about-hunters-message': {
+            'en_GB' : 'he location of our systems makes them favorite place for Pirates, Smugglers and Bounty Hunters. Are you into that passion and shiny coins? You\'ve come to the right place! We were never against smuggling. We will always support a fellow  Bounty Hunter. If you are pirate – you are always welcome, especially with big tasty bounty on your head ;)'
+        },
+        'id:about-builders-header': {
+            'en_GB' : 'We are Builders'
+        },
+        'id:about-builders-message': {
+            'en_GB' : 'We build our future together! We are capturing nearest systems and relentlessly expanding our influence. We are determined to control every settlement in our systems. Under guidance of our wise Dictaror we are building a better world!'
+        },
+        'id:about-protectors-header': {
+            'en_GB' : 'We are Protectors'
+        },
+        'id:about-protectors-message': {
+            'en_GB' : 'Every one joined the EG Union may count on our support. We are providing protection and support in all our systems. If you got yourself into “trouble” – we are there on the first call. We will never abandon our pilots!'
+        },
+
+        'id:features-header': {
+            'en_GB' : 'Advantages of EG Union'
+        },
+        'id:features-territory-header': {
+            'en_GB' : 'Our territory'
+        },
+        'id:features-1': {
+            'en_GB' : 'More than 200 pilots united with one purpose! It\'s fun!'
+        },
+        'id:features-2': {
+            'en_GB' : 'Different missions from our fraction\'s command.'
+        },
+        'id:features-3': {
+            'en_GB' : 'Playing in any regime (Open, Solo, Private Group)'
+        },
+        'id:features-4': {
+            'en_GB' : 'All kinds of events'
+        },
+        'id:features-5': {
+            'en_GB' : 'Cooperative fights and travels'
+        },
+        'id:features-6': {
+            'en_GB' : 'Game tips and advices'
+        },
+        'id:features-7': {
+            'en_GB' : 'Opportunity for every one to grow in ranks'
+        },
+
+        'id:news-header': {
+            'en_GB' : 'News'
+        },
+        'id:news-newspaper-header': {
+            'en_GB' : 'Euryale Chronicles'
+        },
+        'id:news-newspaper-link': {
+            'en_GB' : 'View all Euryale Chronicles (Russian)'
+        },
+        'id:news-video-1-header': {
+            'en_GB' : '15.05.3302 April Digest. News Release.'
+        },
+        'id:news-video-2-header': {
+            'en_GB' : '01.04.3302 March Digest. News Release.'
+        },
+        'id:news-video-3-header': {
+            'en_GB' : '10.03.3302 "Milky Way" Expedition - Sagittarius A* meeting'
+        },
+
+        'id:cmdrs-header': {
+            'en_GB' : 'EG Pilots'
+        },
+        'id:cmdrs-1-role': {
+            'en_GB' : 'Dictator Counsellor'
+        },
+        'id:cmdrs-1-feature': {
+            'en_GB' : 'Strategy, planning and tasks'
+        },
+        'id:cmdrs-2-role': {
+            'en_GB' : 'Senior officer'
+        },
+        'id:cmdrs-2-feature': {
+            'en_GB' : 'Game mechanics'
+        },
+        'id:cmdrs-3-role': {
+            'en_GB' : 'Special Correspondent'
+        },
+        'id:cmdrs-3-feature': {
+            'en_GB' : 'News about EG Union surroundings'
+        },
+        'id:cmdrs-4-role': {
+            'en_GB' : 'Explorer'
+        },
+        'id:cmdrs-4-feature': {
+            'en_GB' : 'Expeditions to the wonders of our Galaxy'
+        },
+    }
+});
