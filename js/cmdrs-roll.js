28bed9d2acab08e888ce73e53c668a7683c1e94b
Руслан Шохирев <ruslan-sh@taxnet.ru>
2016-08-11 14:08:16 +0300

    Дополнен список офицеров

--- js/cmdrs-roll.js
+++ js/cmdrs-roll.js
@@ -1,19 +1,21 @@
 var cmdrs = [
-	"Nirgam",
+	// approved
 	"AL TARF",
-	"H-Arck",
-	"Rzhevski [EG]",
+	"Etsu Togenada",
 	"frolik",
-	"MoHax",
-	"Sover",
 	"GIF",
-	"Pite [EG]",
-	"Star Falcon",
-	"Kosh_EG",
+	"H-Arck",
 	"Kenhen63",
+	"Kosh_EG",
+	"MoHax",
+	"Nirgam",
+	"Pite [EG]",
+	"Rzhevski [EG]",
+	"Sover",
+	"Star Falcon",
 	"Zipun",
 	"Zybr78_RUS",
-	"Etsu Togenada"
+	//new
 ];
 
 function getCmdrIndexs(count){
